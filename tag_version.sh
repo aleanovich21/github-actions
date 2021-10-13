74b2af98317d69fb93312aa078c50f0d4acb9b1c
Andrei Leanovich <andrei.leanovich@wsc-247-71b.icx.local>
2021-10-13 11:31:22 +0300

    Added tag

--- tag_version.sh
+++ tag_version.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-git tag
-#git describe --tags $(git rev-list --tags --max-count=1) | sed -e 's/^v//' > PROJECT_TAG.txt
+git describe --tags $(git rev-list --tags --max-count=1) | sed -e 's/^v//' > PROJECT_TAG.txt
 
